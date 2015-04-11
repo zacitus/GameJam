7587c2e975da0e42878797446311835643118073
JordanKnows <jdb140330@utdallas.edu>
2015-04-11 17:36:14 -0500

    Rocket Shooting and Player LookAt2D
    Nothing works, be careful.

---
+++ Assets/Scripts/AutoDestroy.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+// use this on particle effect prefabs when they need to be removed from the scene after playing
+
+private var ps : ParticleSystem;
+
+function Start(){
+	ps = GetComponent(ParticleSystem);
+}
+
+function Update(){
+	if(ps){
+		if(!ps.IsAlive()){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/CannonProjectileScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var rocketExplosion : GameObject;
+var rocketSpeed : float = 3.0f;
+var rocketDamage : float = 100.0;
+var rocketTarget : Transform;
+
+function Update () {
+	transform.Translate(Vector3.forward * Time.deltaTime * rocketSpeed);
+	if(rocketTarget){
+		transform.LookAt(rocketTarget);
+	}else{
+		Destroy(gameObject);
+	}
+}
+
+function OnTriggerEnter(other : Collider){
+	if(other.gameObject.tag == "Player2"){
+		//var m : int = Random.Range(0,10);
+		//other.gameObject.SendMessage("TakeDamage", cannonDamage + m, SendMessageOptions.DontRequireReceiver);
+		Explode();
+	}
+}
+
+function Explode(){
+	Instantiate(rocketExplosion, transform.position, Quaternion.identity);
+	Destroy(gameObject);
+}
