5e063aeed8c6e9bb9556fac2dcdd8ade23a64276
JordanKnows <jdb140330@utdallas.edu>
2015-04-11 10:15:51 -0500

    Good Morning
    Linked Player1 to Movement Script.
    Made Player2, linked to Aim Script.
    Made Bouding Area for Player1 and 2.
    Input Managed for two controllers.

---
+++ Assets/Scripts/AimScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimScript : MonoBehaviour {
+
+	public GameObject playerTwoObject;
+	private float upDownAimInput = 0.0f;
+	private float leftRightAimInput = 0.0f;
+	private float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//stores input by predefined axis into -1 to 1 * speed
+		if(playerTwoObject){
+			leftRightAimInput = Input.GetAxis ("Horizontal2") * speed * Time.deltaTime;
+			upDownAimInput = Input.GetAxis ("Vertical2") * speed * Time.deltaTime;
+			
+			playerTwoObject.transform.Translate (leftRightAimInput, upDownAimInput, 0);
+		}
+	}
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class MovementScript : MonoBehaviour {
 
-	public GameObject playerObject ;
-	private Rigidbody playerBody;
+	public GameObject playerOneObject;
+	//private Rigidbody playerBody;
 	private float upDownMovementInput = 0.0f;
 	private float leftRightMovementInput = 0.0f;
-	private float speed = 1000.0f;
-	private float screenY = 0.0f;
-	private float screenX = 0.0f;
-	private float movementLimitX = 6.0f;
-	private float movementLimitY = 2.0f;
-	private float movementLimitYOffset = 2.0f;
+	private float speed = 10.0f;
+
+	//private float screenY = 0.0f;
+	//private float screenX = 0.0f;
+	//private float movementLimitX = 8.0f;
+	//private float movementLimitY = 2.0f;
+	//private float movementLimitYOffset = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -33,32 +34,40 @@ public class MovementScript : MonoBehaviour {
 	void FixedUpdate () {
 
 		//stores input by predefined axis into -1 to 1 * speed
-		leftRightMovementInput = Input.GetAxis ("Horizontal") * speed;
-		upDownMovementInput = Input.GetAxis ("Vertical") * speed;
+		if(playerOneObject){
+			leftRightMovementInput = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+			upDownMovementInput = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+
+			playerOneObject.transform.Translate (leftRightMovementInput, upDownMovementInput, 0);
+		}
 
 		//if the player object is within the bounds the player can move, else the player is only allowed to move the one direction dictated by using positive or negative Mathf.Abs
+		/*
 		if (playerObject.transform.position.y > -movementLimitY-movementLimitYOffset && playerObject.transform.position.y < movementLimitY-movementLimitYOffset) {
 			//Debug.Log (playerObject.transform.position.y);
-			playerObject.transform.Translate (0, upDownMovementInput * Time.deltaTime, 0);
-			//playerBody.AddForce(0, upDownMovementInput * Time.deltaTime, 0);
+
+			playerObject.transform.Translate (0, upDownMovementInput, 0);
+			//playerBody.AddForce(0, upDownMovementInput, 0);
 		}else if (playerObject.transform.position.y <= -movementLimitY-movementLimitYOffset){
-			playerObject.transform.Translate (0, Mathf.Abs(upDownMovementInput) * Time.deltaTime, 0);
-			//playerBody.AddForce(0, Mathf.Abs(upDownMovementInput) * Time.deltaTime, 0);
+			playerObject.transform.Translate (0, Mathf.Abs(upDownMovementInput), 0);
+			//playerBody.AddForce(0, Mathf.Abs(upDownMovementInput), 0);
 		}else if (playerObject.transform.position.y >= movementLimitY-movementLimitYOffset){
-			playerObject.transform.Translate (0, -Mathf.Abs(upDownMovementInput) * Time.deltaTime, 0);
-			//playerBody.AddForce(0, -Mathf.Abs(upDownMovementInput) * Time.deltaTime, 0);
+			playerObject.transform.Translate (0, -Mathf.Abs(upDownMovementInput), 0);
+			//playerBody.AddForce(0, -Mathf.Abs(upDownMovementInput), 0);
 		}
 
 		if (playerObject.transform.position.x > -movementLimitX && playerObject.transform.position.x < movementLimitX) {
 			//Debug.Log (playerObject.transform.position.x);
-			playerObject.transform.Translate (leftRightMovementInput * Time.deltaTime, 0, 0);
-			//playerBody.AddForce(leftRightMovementInput * Time.deltaTime, 0, 0);
+
+			playerObject.transform.Translate (leftRightMovementInput, 0, 0);
+			//playerBody.AddForce(leftRightMovementInput, 0, 0);
 		}else if (playerObject.transform.position.x <= -movementLimitX){
-			playerObject.transform.Translate (Mathf.Abs(leftRightMovementInput) * Time.deltaTime, 0, 0);
-			//playerBody.AddForce(Mathf.Abs(leftRightMovementInput) * Time.deltaTime, 0, 0);
+			playerObject.transform.Translate (Mathf.Abs(leftRightMovementInput), 0, 0);
+			//playerBody.AddForce(Mathf.Abs(leftRightMovementInput), 0, 0);
 		}else if (playerObject.transform.position.x >= movementLimitX){
-			playerObject.transform.Translate (-Mathf.Abs(leftRightMovementInput) * Time.deltaTime, 0, 0);
-			//playerBody.AddForce(-Mathf.Abs(leftRightMovementInput) * Time.deltaTime, 0, 0);
+			playerObject.transform.Translate (-Mathf.Abs(leftRightMovementInput), 0, 0);
+			//playerBody.AddForce(-Mathf.Abs(leftRightMovementInput), 0, 0);
 		}
+		*/
 	}
 }
